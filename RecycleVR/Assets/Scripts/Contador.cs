70f56f9eb9de4c8f212ee0321b7690a9cba4f94d
itognetti <itognetti@itba.edu.ar>
2023-11-10 15:59:55 -0300

    Add score sound

--- RecycleVR/Assets/Scripts/Contador.cs
+++ RecycleVR/Assets/Scripts/Contador.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Contador : MonoBehaviour
 {
     public int contadorColisiones = 0;
-    public string caracteresEspecificos = "Bottle"; // Caracteres específicos a buscar en el nombre.
+    public string caracteresEspecificos = "Bottle";
     public TextMesh textoPuntaje;
 
+    public AudioSource audioSource;
 
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.name.Contains(caracteresEspecificos)){
+            contadorColisiones++;
+            textoPuntaje.text = contadorColisiones.ToString();
+            Debug.Log("Colisiones: " + contadorColisiones);
 
-            // Verifica si el nombre del objeto 2 contiene los caracteres específicos.
-            if (other.gameObject.name.Contains(caracteresEspecificos))
-            {
-                contadorColisiones++;
-                textoPuntaje.text = contadorColisiones.ToString();
-                Debug.Log("Colisiones: " + contadorColisiones);
+            // Reproduce el sonido de puntuación.
+            if (audioSource != null){
+                audioSource.Play();
             }
+        }
     }
 }
